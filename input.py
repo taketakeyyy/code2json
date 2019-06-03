fa8637f7ee03b608d185c3aa87901a06f8c2f4b4
taketakeyyy <taketakeyyy@gmail.com>
2019-06-03 16:04:29 +0900

    add: main code

---
+++ input.py
@@ -0,0 +1,10 @@
+class Hoge():
+    def __init__(self, V):
+        """ hogehoge
+        This is hoge class.
+        """
+        pass
+
+    def hoge(self):
+        """ hoge method """
+        pass
